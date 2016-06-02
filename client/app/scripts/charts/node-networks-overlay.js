c5870a10aa3f44a0fd41cbc58718c7b77c6fafb1
Simon Howe <footless@gmail.com>
2016-06-02 09:29:33 +0200

    Handle edge case by making line longer

--- client/app/scripts/charts/node-networks-overlay.js
+++ client/app/scripts/charts/node-networks-overlay.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import d3 from 'd3';
-import _ from 'lodash';
 import { List as makeList } from 'immutable';
 import { getNodeColor } from '../utils/color-utils';
 import { isContrastMode } from '../utils/contrast-utils';
@@ -10,18 +9,18 @@ const h = 5;
 const padding = 0.05;
 const rx = 1;
 const ry = rx;
-const perRow = 4;
 
 function NodeNetworksOverlay({size, orientation = 'down', stack, networks = makeList()}) {
   const offset = orientation === 'up' ? -size * 0.5 - h - 9 : size + 6;
+  const w = Math.max(size, (size / 4) * networks.size);
   const x = d3.scale.ordinal()
-    .domain(_.range(Math.min(perRow, networks.size)))
-    .rangeBands([size * -0.5, size * 0.5], padding, 0);
+    .domain(networks.map((n, i) => i).toJS())
+    .rangeBands([w * -0.5, w * 0.5], padding, 0);
 
   const bars = networks.map((n, i) => (
     <rect
-      x={x(i % perRow)}
-      y={offset + Math.floor(i / perRow) * (h + 2)}
+      x={x(i)}
+      y={offset}
       width={x.rangeBand()}
       height={h}
       rx={rx}
@@ -37,7 +36,7 @@ function NodeNetworksOverlay({size, orientation = 'down', stack, networks = make
   let transform = '';
   if (stack) {
     const contrastMode = isContrastMode();
-    const [dx, dy] = contrastMode ? [0, 8] : [0, 8];
+    const [dx, dy] = contrastMode ? [0, 8] : [0, 0];
     transform = `translate(${dx}, ${dy * -1.5})`;
   }
 
